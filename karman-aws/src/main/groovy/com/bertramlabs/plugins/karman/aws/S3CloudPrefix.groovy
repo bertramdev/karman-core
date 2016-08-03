031a1939175ab4aa6d7ceb11f1f4e514d0406a15
David Estes <davydotcom@gmail.com>
2016-08-03 11:23:48 -0400

    version bump

---
+++ karman-aws/src/main/groovy/com/bertramlabs/plugins/karman/aws/S3CloudPrefix.groovy
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bertramlabs.plugins.karman.aws
+
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.Headers
+import com.amazonaws.services.s3.model.*
+import com.bertramlabs.plugins.karman.CloudFile
+import com.bertramlabs.plugins.karman.Directory
+import com.bertramlabs.plugins.karman.util.ChunkedInputStream
+
+class S3CloudPrefix extends CloudFile {
+
+	S3Directory parent
+
+	@Override
+	InputStream getInputStream() {
+		return null
+	}
+
+	@Override
+	void setInputStream(InputStream is) {
+
+	}
+
+	@Override
+	OutputStream getOutputStream() {
+		return null
+	}
+
+	@Override
+	String getText(String encoding) {
+		return null
+	}
+
+
+	@Override
+	byte[] getBytes() {
+		return new byte[0]
+	}
+
+	@Override
+	void setText(String text) {
+
+	}
+
+	@Override
+	void setBytes(Object bytes) {
+
+	}
+
+	@Override
+	Long getContentLength() {
+		return null
+	}
+
+	@Override
+	String getContentType() {
+		return null
+	}
+
+	@Override
+	void setContentType(String contentType) {
+
+	}
+
+	@Override
+	Boolean exists() {
+		return true
+	}
+
+	@Override
+	Boolean isFile() {
+		return false
+	}
+
+	@Override
+	Boolean isDirectory() {
+		return true
+	}
+
+	def save(acl) {
+		throw new Exception("Not Implemented for a directory")
+	}
+
+	@Override
+	def delete() {
+		return null
+	}
+
+	@Override
+	void setMetaAttribute(Object key, Object value) {
+
+	}
+
+	@Override
+	def getMetaAttribute(Object key) {
+		return null
+	}
+
+	@Override
+	def getMetaAttributes() {
+		return null
+	}
+
+	@Override
+	void removeMetaAttribute(Object key) {
+
+	}
+}
