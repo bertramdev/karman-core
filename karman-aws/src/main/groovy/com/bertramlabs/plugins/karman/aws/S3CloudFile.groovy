5357eb2fc30b2f2a1538b797301823912eedd84e
David Estes <davydotcom@gmail.com>
2016-08-15 17:44:55 -0400

    fixing content length bug potentially

--- karman-aws/src/main/groovy/com/bertramlabs/plugins/karman/aws/S3CloudFile.groovy
+++ karman-aws/src/main/groovy/com/bertramlabs/plugins/karman/aws/S3CloudFile.groovy
@@ -220,7 +220,7 @@ class S3CloudFile extends CloudFile {
             assert writeableStream
             Long contentLength = object.objectMetadata.contentLength
             setMetaAttribute(Headers.S3_CANNED_ACL, acl)
-			if(contentLength && contentLength <= 4*1024l*1024l*1024l && parent.provider.forceMultipart == false) {
+			if(contentLength != null && contentLength <= 4*1024l*1024l*1024l && parent.provider.forceMultipart == false) {
 				s3Client.putObject(parent.name, name, writeableStream, object.objectMetadata)
 			} else {
 				saveChunked()
