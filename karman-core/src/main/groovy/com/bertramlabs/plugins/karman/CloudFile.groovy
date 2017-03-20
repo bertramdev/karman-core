dd9ced54cdaaed30a3feeced2307a289267cb5a2
David Estes <davydotcom@gmail.com>
2017-03-20 09:44:19 -0400

    tweaking cache stream stoer

--- karman-core/src/main/groovy/com/bertramlabs/plugins/karman/CloudFile.groovy
+++ karman-core/src/main/groovy/com/bertramlabs/plugins/karman/CloudFile.groovy
@@ -118,14 +118,13 @@ abstract class CloudFile implements CloudFileInterface {
 		File tempFile
 		OutputStream out
 		 try {
-			 if(provider.tempDir || KarmanConfigHolder.getTempDir()) {
-				 File tempDirFile = new File(provider.tempDir ?: KarmanConfigHolder.getTempDir())
-				 if(!tempDirFile.exists()) {
-					 tempDirFile.mkdirs()
-				 }
-				 temporaryDirectory = tempDirFile.toPath()
+			 
+			 File tempDirFile = new File(provider.tempDir ?: KarmanConfigHolder.getTempDir() ?: '/tmp')
+			 if(!tempDirFile.exists()) {
+				 tempDirFile.mkdirs()
 			 }
-
+			 temporaryDirectory = tempDirFile.toPath()
+		 
 			 Path tempFilePath = Files.createTempFile(temporaryDirectory,name,null)
 			 tempFile = tempFilePath.toFile()
 
